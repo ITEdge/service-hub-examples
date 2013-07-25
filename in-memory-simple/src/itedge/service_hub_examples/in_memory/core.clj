1e35edd201f56a131644de445c385c11e6a61c7f
Peter Hanak <hanakpet@gmail.com>
2013-07-25 12:31:19 +0200

    Addition of parent project and in-memory-simple example

---
+++ in-memory-simple/src/itedge/service_hub_examples/in_memory/core.clj
@@ -0,0 +1,34 @@
+(ns itedge.service-hub-examples.in-memory.core
+  (:use itedge.service-hub.core.handlers
+        itedge.service-hub.core.services
+        compojure.core
+        itedge.service-hub.http-ring.middleware.translate-params
+        [ring.middleware params
+                         keyword-params
+                         nested-params
+                         file-info
+                         resource])
+  (:require [clojure.string :as string]
+            [ring.adapter.jetty :as jetty]
+            [itedge.service-hub-examples.in-memory.handlers :as handlers]
+            [itedge.service-hub-examples.in-memory.services :as services]
+            [compojure.handler :as handler]
+            [itedge.service-hub.core.util :as util]
+            [itedge.service-hub.http-ring.routes-util :as routes-util]
+            [itedge.service-hub.http-ring.content-util :as content-util]))
+
+(def product-service (services/->ProductService))
+
+(defroutes app-routes
+  (routes-util/scaffold-crud-routes "/products" product-service :id read-string content-util/craft-edn-response))
+
+(def app
+  (-> app-routes
+    (wrap-translate-params)
+    (wrap-keyword-params)
+    (wrap-nested-params)
+    (wrap-params)
+    (wrap-resource "public")
+    (wrap-file-info)))
+
+(defonce server (jetty/run-jetty app {:port 3000 :join? false}))
