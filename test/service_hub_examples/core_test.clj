cc6ceacc0756049e59dbad43781b54250d4739d4
JHE <jan.herich@gmail.com>
2013-07-25 16:28:47 +0200

    remove unnecessary files

--- test/service_hub_examples/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns service-hub-examples.core-test
-  (:require [clojure.test :refer :all]
-            [service-hub-examples.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
