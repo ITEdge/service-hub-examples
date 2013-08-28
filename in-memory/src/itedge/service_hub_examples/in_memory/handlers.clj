48bb7bc5bf0c4843dba2b471d56e33a7376aaa63
jhe <jan.herich@gmail.com>
2013-08-28 21:55:35 +0200

    use sets instead of vector for relations

--- in-memory/src/itedge/service_hub_examples/in_memory/handlers.clj
+++ in-memory/src/itedge/service_hub_examples/in_memory/handlers.clj
@@ -2,12 +2,12 @@
   (:require [itedge.service-hub.core.handlers :refer :all]
             [itedge.service-hub.core.handlers-memory :as handlers-memory]))
 
-(def role-handler (handlers-memory/create-memory-handler [{:rolename :admin :users [1]}
-                                                          {:rolename :user :users [1]}] :id))
+(def role-handler (handlers-memory/create-memory-handler [{:rolename :admin :users #{1}}
+                                                          {:rolename :user :users #{1}}] :id))
 
 (def user-handler (handlers-memory/create-memory-handler [{:username "admin" 
                                                            :password "$2a$10$K83DHEqRZ7ZCpq0HK49lj.4XW/EvMw4XPQPSrIxWNVPsOhwFk8bHe"
-                                                           :roles [1 2]}] :id))
+                                                           :roles #{1 2}}] :id))
 
 (def product-handler (handlers-memory/create-memory-handler [{:product_name "product-1" :price 10 :order_number "P1"}
                                                              {:product_name "product-2" :price 20 :order_number "P2"}] :id))
