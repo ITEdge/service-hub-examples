8e3ea2cc1d41df4e77a8a6a6a5d5eb8c3263b62b
jhe <jan.herich@gmail.com>
2013-09-04 19:19:30 +0200

    Updated gitignore and sample data set changes

--- datomic/src/itedge/service_hub_examples/datomic/handlers.clj
+++ datomic/src/itedge/service_hub_examples/datomic/handlers.clj
@@ -1,7 +1,16 @@
 (ns itedge.service-hub-examples.datomic.handlers
-  (:require [itedge.service-hub.core.handlers :refer :all]
+  (:require [datomic.api :as d :refer [q db entity create-database connect transact shutdown]]
+            [itedge.service-hub.core.handlers :refer :all]
             [itedge.service-hub.persistence-datomic.handlers-datomic :as handlers-datomic]))
 
-(def user-handler (handlers-datomic/create-handler nil #{:user/username :user/password :user/roles}))
-(def role-handler (handlers-datomic/create-handler nil #{:role/users :role/rolename}))
-(def product-handler (handlers-datomic/create-handler nil #{:product/name :product/order-code :product/price}))
+(def uri "datomic:mem://sample-datomic")
+(create-database uri)
+(def conn (connect uri))
+(def schema-tx (read-string (slurp "resources/schema.edn")))
+@(d/transact conn schema-tx)
+(def data-tx (read-string (slurp "resources/data.edn")))
+@(d/transact conn data-tx)
+
+(def user-handler (handlers-datomic/create-handler conn #{:user/username :user/password :user/roles}))
+(def role-handler (handlers-datomic/create-handler conn #{:role/users :role/rolename}))
+(def product-handler (handlers-datomic/create-handler conn #{:product/name :product/order-code :product/price}))
